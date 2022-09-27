00dbf855fd64f98d3f162750d710e177ee67e7b6
Striker2785 <64331077+Striker2785@users.noreply.github.com>
2022-09-27 17:24:24 -0600

    Add files via upload

---
+++ Prueba.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include "Obvio.hpp"
+#include "errno.h"
+#include <iostream>
+#include <stdio.h>
+
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    Image img;
+    img.read("Images/in.bmp");
+    img.horizontalScale(2.0);
+    img.write("Images/out.bmp");
+    img.clear();
+    //string path = "Images/K-on.jpg";
+    //Mat img = imread(path);
+    //imshow("Image", img);
+    //waitKey(0);
+    return 0; 
+    
+}   
