1a41a90294653f8ee4a50fb5c37a7026846bf03c
Striker2785 <64331077+Striker2785@users.noreply.github.com>
2022-09-29 19:53:08 -0600

    Subiendo el cliente y servidor

---
+++ Cliente.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string ubicacion, filtro, fact;
+    //cout << "Ingrese la ubicacion de la imagen: \r\n";
+    //getline(cin, ubicacion);
+
+    //	Create a socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        return 1;
+    }
+
+    //	Create a hint structure for the server we're connecting with
+    int port = 54000;
+    string ipAddress = "127.0.0.1";
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+    //	Connect to the server on the socket
+    int connectRes = connect(sock, (sockaddr*)&hint, sizeof(hint));
+    if (connectRes == -1)
+    {
+        return 1;
+    }
+
+    //	While loop:
+    char buf[4096];
+    char fekts[4096];
+    
+
+
+    do {
+        //		Enter lines of text
+        cout << "Ingrese la ubicacion de la imagen: \r\n";
+        getline(cin, ubicacion);
+        //cout << "> ";
+        //getline(cin, ubicacion);
+
+        //		Send to server
+        int sendRes = send(sock, ubicacion.c_str(), ubicacion.size() + 1, 0);
+        if (sendRes == -1)
+        {
+            cout << "Could not send to server! Whoops!\r\n";
+            continue;
+        }
+
+        //		Wait for response
+        memset(buf, 0, 4096);
+        memset(fekts,0,4096);
+        int bytesReceived = recv(sock, buf, 4096, 0);
+        if (bytesReceived == -1)
+        {
+            cout << "There was an error getting response from server\r\n";
+        }
+        
+            //		Display response
+
+        cout << "SERVER> " << string(buf, bytesReceived) << "\r\n";
+
+        cout << "> ";
+        getline(cin, filtro);
+        sendRes = send(sock, filtro.c_str(), filtro.size()+1, 0);   
+       
+        int factores = recv(sock, fekts, 4096, 0);
+
+        cout << "SERVER> " << string(fekts, factores) << "\r\n"; 
+    
+
+        cout << "> ";
+        getline(cin, fact);
+        sendRes = send(sock, fact.c_str(), fact.size()+1, 0);
+
+
+    } while(true);
+
+    //	Close the socket
+    close(sock);
+
+    return 0;
+}
---
+++ Servidor.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <ImageProcessing.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == -1)
+    {
+        cerr << "Cant create a socket";
+        return -1;
+    }
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000);
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+
+       bind(listening, (sockaddr*)&hint, sizeof(hint));
+ 
+    // Tell Winsock the socket is for listening
+    listen(listening, SOMAXCONN);
+ 
+    // Wait for a connection
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+ 
+    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+ 
+    char host[NI_MAXHOST];      // Client's remote name
+    char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
+ 
+    memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+    memset(service, 0, NI_MAXSERV);
+ 
+    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+    {
+        cout << host << " connected on port " << service << endl;
+    }
+    else
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+    }
+ 
+    // Close listening socket
+    close(listening);
+ 
+    // While loop: accept and echo message back to client
+    char buf[4096];
+    char buf2[4096];
+    char fact[4096];
+    
+    char *path;
+    while (true)
+    {
+        memset(buf, 0, 4096);
+        memset(buf2, 0, 4096);
+        memset(fact, 0, 4096);
+    
+        char *err = "No se pudo encontrar la imagen\n";
+        // Wait for client to send data
+        int bytesReceived = recv(clientSocket, buf, 4096, 0);
+        if (bytesReceived == -1)
+        {
+            cerr << "Error in recv(). Quitting" << endl;
+            break;
+        }
+ 
+        if (bytesReceived == 0)
+        {
+            cout << "Client disconnected " << endl;
+            break;
+        }
+
+        ImageProcessing image;
+        image.read(buf);
+        if(image.image.empty()){
+            send(clientSocket, err, strlen(err)+1,0);
+            cout << "Imagen no encontrada\n";
+            break;
+        }
+        char *men = "Eliga el filtro a aplicar\n 1: Gaussian Blur\n 2: Escala de grises\n 3: Control de brillo\n 4: Correccion de gama\n";
+        char *factor = "Indique el factor de brillo que desea aplicar: \n";
+        path = buf;
+        //cout << string(buf, 0, bytesReceived) << endl;
+
+       
+        
+        send(clientSocket, men, strlen(men) + 1, 0);
+
+        int bytes2 = recv(clientSocket, buf2, 4096, 0);
+
+        if (bytes2 == -1){
+            
+            cerr << "Error al recibir" << endl;
+            break;
+        }
+
+        if (bytes2 == 0){
+            
+            cout << "Cliente desconectado " << endl;
+            
+        }
+
+        if(string(buf2, 0, bytes2)=="1"){
+            
+            image.gaussianBlur();
+            break;
+        }
+
+        if(string(buf2, 0, bytes2)=="2"){
+
+            image.gray_scale();
+            break;
+        }
+
+        if(string(buf2, 0, bytes2)=="3"){
+
+            send(clientSocket, factor, strlen(factor)+1, 0);
+            int facts = recv(clientSocket, fact, 4096, 0);
+            image.brightControl(stoi(fact));
+            break;
+            //cout << fact << "\n";
+
+
+        }
+
+
+
+        //image.matToArray(buf);
+        //image.prueba();
+        //image.arrayToMat();
+        //image.gray_scale();
+        //image.gaussianBlur();
+        //image.brightControl(200);
+        
+
+        // Echo message back to client
+        //send(clientSocket, buf, bytesReceived + 1, 0);
+    }
+ 
+    // Close the socket
+    close(clientSocket);
+ 
+
+    return 0;
+}
